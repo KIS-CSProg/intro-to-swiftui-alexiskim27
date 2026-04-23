cd15e7268c3adf4b1f1236a9f98adcbeb2e89ce2
Alexis Kim <alexiskim27@student.kis.or.kr>
2026-04-23 13:03:57 +0900

    Create ChatPrototype Project

---
+++ AlexisKim_ChatPrototype/AlexisKim_ChatPrototype/AlexisKim_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlexisKim_ChatPrototypeApp.swift
+//  AlexisKim_ChatPrototype
+//
+//  Created by 김승연 on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct AlexisKim_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
